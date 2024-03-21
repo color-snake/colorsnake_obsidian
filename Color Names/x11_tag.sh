a9eb0102587a288bf67738e9f7eddef98c662eca
The SunShining <thesunshiningmin@gmail.com>
2024-03-21 11:47:53 -0500

    x11 take 2

--- Color Names/x11_tag.sh
+++ Color Names/x11_tag.sh
@@ -23,18 +23,22 @@ def update_tags_in_markdown_files(directory, csv_file):
                 lines = file.readlines()
 
             existing_tags = set()
+            tags_updated = False
             for line in lines:
                 if line.strip().startswith("- Color/Tag/"):
                     existing_tags.add(line.strip())
+                if tag_to_add in line:
+                    tags_updated = True
 
             hex_value = file_name.split('.')[0].lower()
-            if hex_value in x11_colors and tag_to_add not in existing_tags:
+            if hex_value in x11_colors and tag_to_add not in existing_tags and not tags_updated:
                 existing_tags.add(tag_to_add)
 
             with open(markdown_file, 'w') as file:
-                file.write("tags:\n")
-                for tag in existing_tags:
-                    file.write(f"  {tag}\n")
+                for line in lines:
+                    file.write(line)
+                if not tags_updated:
+                    file.write(f"  {tag_to_add}\n")
 
 if __name__ == "__main__":
     directory = "."  # Change this to the directory where your Markdown files are located
