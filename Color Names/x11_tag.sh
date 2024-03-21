4e4e01140e923f773443261cad86b2ca02401ca1
The SunShining <thesunshiningmin@gmail.com>
2024-03-21 12:01:48 -0500

    Update x11_tag.sh

--- Color Names/x11_tag.sh
+++ Color Names/x11_tag.sh
@@ -16,9 +16,12 @@ def update_tags_in_markdown_files(directory, csv_file):
     x11_colors = read_colors_from_csv(csv_file)
     tag_to_add = "- Color/Tag/x11"
     
+    print("Colors from x11.csv:", x11_colors)
+    
     for file_name in os.listdir(directory):
         if file_name.endswith('.md'):
             hex_value = os.path.splitext(file_name)[0].lower()
+            print("Processing file:", file_name, "with hex value:", hex_value)
             if hex_value in x11_colors:
                 markdown_file = os.path.join(directory, file_name)
                 with open(markdown_file, 'r') as file:
